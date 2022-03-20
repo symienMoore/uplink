c34a1dd1291c17b886bcaefad1349fdaa93ef19e
symi <symien.moore@gmail.com>
2022-03-20 15:33:35 -0400

    wip

--- warbird/src/main/kotlin/com/uplink/warbird/models/User.kt
+++ warbird/src/main/kotlin/com/uplink/warbird/models/User.kt
@@ -1,6 +1,7 @@
-package com.example.warbird.models
+package com.uplink.warbird.models
 
 import javax.annotation.processing.Generated
+import javax.persistence.Column
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
@@ -11,11 +12,23 @@ data class User(
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     val id: Long,
+
+    @Column
     val first_name: String,
+
+    @Column
     val last_name: String,
+
+    @Column
     val password: String,
+
+    @Column
     val username: String,
+
+    @Column
     val email: String,
+
+    @Column
     val profile_picture: String
 ) {
 }
