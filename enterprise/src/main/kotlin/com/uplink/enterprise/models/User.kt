78c204ee8b28aa23498b4fd15d9e70dabbc752a4
symi <symien.moore@gmail.com>
2022-05-04 13:56:49 -0400

    small update

--- enterprise/src/main/kotlin/com/uplink/enterprise/models/User.kt
+++ enterprise/src/main/kotlin/com/uplink/enterprise/models/User.kt
@@ -3,6 +3,7 @@ package com.uplink.enterprise.models
 import org.bson.types.ObjectId
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
+import java.time.LocalDateTime
 
 @Document
 data class User (
@@ -11,7 +12,9 @@ data class User (
     val name: String = "",
     val username: String = "",
     val email: String = "",
-    val password: String = ""
+    val password: String = "",
+    val createdDate: LocalDateTime = LocalDateTime.now(),
+    val modifiedDate: LocalDateTime = LocalDateTime.now()
 ){
 
 //    val id: String = ""
